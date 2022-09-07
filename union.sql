24e042c4e43c72d819b13d98d1819dd1e527d2b2
paullaster <paullasterokoth98@gmail.com>
2022-09-07 12:31:52 +0300

    operation on a combined dataset

--- union.sql
+++ union.sql
@@ -28,3 +28,26 @@ UNION ALL
 SELECT *
 FROM accounts
 WHERE name = 'Disney'
+
+/*
+Perform the union in your 
+first query (under the Appending 
+Data via UNION header) in a common table expression 
+and name it double_accounts. 
+Then do a COUNT the number of times a 
+name appears in the double_accounts table
+*/
+WITH double_accounts AS (
+SELECT *
+FROM accounts
+
+UNION ALL
+
+SELECT *
+FROM accounts
+)
+SELECT
+name,
+COUNT(*)
+FROM double_accounts
+GROUP BY name
