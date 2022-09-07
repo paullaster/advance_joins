41f0ab4d761b14d83db29c82f950aa3950b5fa1c
paullaster <paullasterokoth98@gmail.com>
2022-09-07 12:18:38 +0300

    union

---
+++ union.sql
@@ -0,0 +1,13 @@
+/*
+Write a query that uses 
+UNION ALL on two instances
+ (and selecting all columns) 
+ of the accounts table.
+*/
+SELECT *
+FROM accounts
+
+UNION ALL
+
+SELECT *
+FROM accounts
